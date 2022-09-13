f4504d7a2e93046b615e8cc6a4ce66201d04091c
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-09-13 14:58:02 +0800

    done with header v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ import logo from '../assets/logo.png';
 import dc from '../assets/discord.png';
 import open_sea from '../assets/opensea.png';
 import twitter from '../assets/twitter.png';
+import short from '../assets/Short.png';
 
 function Header() {
     return(
-        
+        <section>
             <header>
                     <img src={logo} className="logo"></img>
                     <ul>
@@ -20,7 +21,19 @@ function Header() {
                         <li><a href="#"><img src={open_sea} className="social"></img></a></li>
                     </ul>
             </header>
-        
+                    <div className="mint">
+                        <div className="textBox">
+                            <p>!TALONG</p>
+                            <h1>WHO WILL RISE</h1>
+                            <h1>ABOVE ALL EGGPLANTS?</h1>
+                            <p>WILL A NEW CLAN EMERGE?</p>
+                            <button className="mint-btn"><a href="#">MINT</a></button>
+                        </div>
+                        <div className="imgBox">
+                            <img src={short} className="short-talong"></img>
+                        </div>
+                    </div>
+        </section>
     )
 }
 
