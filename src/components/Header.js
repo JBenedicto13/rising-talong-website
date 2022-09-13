cd90d88029db676028885f784f6cef9f6697609f
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-09-13 12:30:47 +0800

    start to work at header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,20 @@ import React from "react";
 import '../styles/header.css';
 import pic1 from '../assets/pic1.jpg';
 
+
 function Header() {
     return(
-        <div className="header">
-            <ul>
-                <li><a className="first">This is haha</a></li>
-                
-            </ul>
-            <img src={pic1} style={{height: "100px", width: "100px"}} />
-            <h1>Hi world!</h1>
-        </div>
+        
+            <header>
+                    <img src="" alt="LOGO" className="logo"></img>
+                    <ul>
+                        <li><a>ABOUT</a></li>
+                        <li><a>ROADMAP</a></li>
+                        <li><a>TEAM</a></li>
+                        <li><a>MINT</a></li>
+                    </ul>
+            </header>
+        
     )
 }
 
