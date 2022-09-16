655bbb5ae4f0510dfa88ce4cf333765a36457654
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-09-16 11:51:26 +0800

    Changing layout of about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,24 +5,30 @@ function About() {
   return (
      <section>
         <div className="about">
+        <div className="img-container">
+            <img className="eggplants" src={eggplants}></img>
+          </div>
           <div className="text-container">
             <h3>WHAT IS RISING EGGPLANTS?</h3>
-            <p>RISING EGGPLANTS IS A COLLECTION OF 
-               2222 EGGPLANTS THAT WILL SOON RISE IN 
-               THE POLYGON BLOCKCHAIN. IT AIMS TO 
-               CONNECT PEOPLE, SUPPORT LOCAL ARTIST,
-               AND PROMOTE SOCIAL INVOLVEMENT. ASIDE
-               FROM BEING A CHILL AND FUN PROJECT, 
-               THE VISION FOR THE COMMUNITY IS FOR IT 
-               TO BE ENGAGED IN SOCIAL, ARTISTIC, AND 
-               FUN ACTIVITIES WITH THE HELP OF 
-               EXPERTS AND COMMUNITY MEMBERS.
-               TOGETHER WITH OUR EGGPLANTS, RISE 
-               WITH US! 
-            </p>
-          </div>
-          <div className="img-container">
-            <img className="eggplants" src={eggplants}></img>
+            <ul>
+              <li>
+                RISING EGGPLANTS IS A COLLECTION OF 2222 EGGPLANTS
+                THAT WILL SOON RISE IN THE POLYGON BLOCKCHAIN.
+              </li>
+              <li>
+                  IT AIMS TO CONNECT PEOPLE, SUPPORT LOCAL ARTIST, 
+                  AND PROMOTE SOCIAL INVOLVEMENT.
+              </li>
+              <li>
+                ASIDE FROM BEING A CHILL AND FUN PROJECT, THE
+                VISION FOR THE COMMUNITY IS FOR IT TO BE ENGAGED 
+                IN SOCIAL, ARTISTIC, AND FUN ACTIVITIES WITH THE 
+                HELP OF EXPERTS AND COMMUNITY MEMBERS.
+              </li>
+              <li>
+                TOGETHER WITH OUR EGGPLANTS, RISE WITH US! &#127814;
+              </li>
+            </ul>
           </div>
         </div>
      </section>
