37743490f04ca520d35986ad7ca15026adfeb222
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-09-16 21:39:41 +0800

    talong map to be position

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Road_map from './components/Road_map';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <About />
+        <Road_map />
         <div>
          
         </div>
---
+++ src/components/Road_map.js
@@ -0,0 +1,75 @@
+import React from "react";
+import '../styles/roadmap.css'
+import bg from '../assets/background.png'
+function Road_map() {
+  return (
+    <div className="main-container"> 
+        <h1 className="title">TALONG MAP</h1>
+      <div className="road-map"> 
+        <div className="branch-container">
+              <div className="branch-1">
+                <h4>BRANCH 1</h4>
+                <p>
+                   I. SETUP DISCORD SERVER
+                   AND SOCMED ACCOUNTS 
+                </p>
+                <p>
+                   II. ESTABLISH TALONG 
+                   COMMUNITY AND PARTNERSHIPS
+                </p>
+                <p>
+                   III. LIST 2222 TALONGS IN 
+                   THE POLYGON BLOCKCHAIN
+                </p>
+              </div>
+              <div className="branch-1">
+                <h4>BRANCH 1</h4>
+                <p>
+                   I. SETUP DISCORD SERVER
+                   AND SOCMED ACCOUNTS 
+                </p>
+                <p>
+                   II. ESTABLISH TALONG 
+                   COMMUNITY AND PARTNERSHIPS
+                </p>
+                <p>
+                   III. LIST 2222 TALONGS IN 
+                   THE POLYGON BLOCKCHAIN
+                </p>
+              </div>
+              <div className="branch-1">
+                <h4>BRANCH 1</h4>
+                <p>
+                   I. SETUP DISCORD SERVER
+                   AND SOCMED ACCOUNTS 
+                </p>
+                <p>
+                   II. ESTABLISH TALONG 
+                   COMMUNITY AND PARTNERSHIPS
+                </p>
+                <p>
+                   III. LIST 2222 TALONGS IN 
+                   THE POLYGON BLOCKCHAIN
+                </p>
+              </div>
+              <div className="branch-1">
+                <h4>BRANCH 1</h4>
+                <p>
+                   I. SETUP DISCORD SERVER
+                   AND SOCMED ACCOUNTS 
+                </p>
+                <p>
+                   II. ESTABLISH TALONG 
+                   COMMUNITY AND PARTNERSHIPS
+                </p>
+                <p>
+                   III. LIST 2222 TALONGS IN 
+                   THE POLYGON BLOCKCHAIN
+                </p>
+              </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+export default Road_map
