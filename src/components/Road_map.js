a89e6ed81a2f81e4b84c9ab0f699af19b7431efd
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-09-17 13:38:39 +0800

    done with about section v1

--- src/components/Road_map.js
+++ src/components/Road_map.js
@@ -8,64 +8,75 @@ function Road_map() {
       <div className="road-map"> 
         <div className="branch-container">
               <div className="branch-1">
-                <h4>BRANCH 1</h4>
-                <p>
-                   I. SETUP DISCORD SERVER
-                   AND SOCMED ACCOUNTS 
-                </p>
-                <p>
-                   II. ESTABLISH TALONG 
-                   COMMUNITY AND PARTNERSHIPS
-                </p>
-                <p>
-                   III. LIST 2222 TALONGS IN 
-                   THE POLYGON BLOCKCHAIN
-                </p>
+                <p>BRANCH 1</p>
+                <ol>
+                  <li>
+                     SETUP DISCORD SERVER
+                     AND SOCMED ACCOUNTS 
+                  </li>
+                  <li>
+                     ESTABLISH TALONG 
+                     COMMUNITY AND PARTNERSHIPS
+                  </li>
+                  <li>
+                     LIST 2222 TALONGS IN 
+                     THE POLYGON BLOCKCHAIN
+                  </li>
+                </ol>
               </div>
-              <div className="branch-1">
-                <h4>BRANCH 1</h4>
-                <p>
-                   I. SETUP DISCORD SERVER
-                   AND SOCMED ACCOUNTS 
-                </p>
-                <p>
-                   II. ESTABLISH TALONG 
-                   COMMUNITY AND PARTNERSHIPS
-                </p>
-                <p>
-                   III. LIST 2222 TALONGS IN 
-                   THE POLYGON BLOCKCHAIN
-                </p>
+              <div className="branch-2">
+                <p>BRANCH 2</p>
+                  <ol start={4}>
+                     <li>
+                        TALONG LOVER MINTING:2 MATIC
+                     </li>
+                     <li>
+                        TALONG LISTED MINTING:2 MATIC
+                     </li>
+                     <li>
+                        PUBLIC MINTING: 5 MATIC
+                     </li>
+                     <li>
+                        THE RISE OF TALONG COMICS
+                     </li>
+                     <li>
+                        RISE OF TROPANG TALONG
+                     </li>
+                     <li>
+                     &#127814; 1~554 TALONGS TALONG RARITY
+                     </li>
+                  </ol>
               </div>
-              <div className="branch-1">
-                <h4>BRANCH 1</h4>
-                <p>
-                   I. SETUP DISCORD SERVER
-                   AND SOCMED ACCOUNTS 
-                </p>
-                <p>
-                   II. ESTABLISH TALONG 
-                   COMMUNITY AND PARTNERSHIPS
-                </p>
-                <p>
-                   III. LIST 2222 TALONGS IN 
-                   THE POLYGON BLOCKCHAIN
-                </p>
+              <div className="branch-3">
+               <p>BRANCH 3</p>
+               <ol start={10}>
+                  <li>
+                     EXECUTION OF TALONG FUSION A
+                     BREEDING &#40; 1111 SOLD &#41;
+                  </li>
+                  <li>
+                     DISBURSEMENT OF UTILITIES
+                     FOR THE HOLDERS
+                  </li>
+                  <li>
+                     INVOLVEMENT TO AGRICULTURAL/  
+                     ENVIRONMENTAL INITIATIVES
+                  </li>
+               </ol>
               </div>
-              <div className="branch-1">
-                <h4>BRANCH 1</h4>
-                <p>
-                   I. SETUP DISCORD SERVER
-                   AND SOCMED ACCOUNTS 
-                </p>
-                <p>
-                   II. ESTABLISH TALONG 
-                   COMMUNITY AND PARTNERSHIPS
-                </p>
-                <p>
-                   III. LIST 2222 TALONGS IN 
-                   THE POLYGON BLOCKCHAIN
-                </p>
+              <div className="branch-4">
+                <p>BRANCH 4</p>
+                  <ol start={13}>
+                     <li>
+                        START OF RISING EGGPLANT V2
+                     </li>
+                     <li>
+                        STAKING WITH MINKA 
+                     </li>
+                     <li>
+                        TALONGTADONG TALONG
+                     </li>
+                  </ol>
               </div>
           </div>
         </div>
