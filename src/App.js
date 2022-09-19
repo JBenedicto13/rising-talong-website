d37ef0ade2d60ac8507483d0df95406ec568ded4
jbenedicto20.jbb@gmail.com <JBenedicto13>
2022-09-19 21:19:22 +0800

    Fix warning for name casing of RoadMap.js, it should be Camel Casing or Snake Casing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
-import Road_map from './components/Road_map';
+import RoadMap from './components/RoadMap';
 import Faqs from './components/Faqs';
+import Team from './components/Team';
 
 function App() {
   return (
     <div className="App ff-bristol">
         <Header />
         <About />
-        <Road_map />
+        <RoadMap />
         <Faqs />
+        <Team />
         <div>
          
         </div>
