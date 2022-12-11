0f5c00a73bacd9e385b2f44b4a142b84f20cf620
jbenedicto20.jbb@gmail.com <JBenedicto13>
2022-12-11 19:03:58 +0800

    Add maintenance on mintpage

--- src/components/MintPage.js
+++ src/components/MintPage.js
@@ -1,17 +1,23 @@
 import React from 'react';
-var mintStyle = { display: "flex", justifyContent: "center" }
+import "../styles/minting.css";
+import logo from '../assets/logo.png';
 
 const MintPage = () => {
   return (
     <div className='mintPage'>
-      <iframe
+      <div className='maintenancediv'>
+        <img src={logo} alt='logo'></img>
+        <h1 id='maintenancetext'>Under Maintenace,<br></br> please comeback later.</h1>
+      </div>
+      
+      {/* <iframe
         style={mintStyle}
         src="https://gateway.ipfscdn.io/ipfs/Qmcine1gpZUbQ73nk7ZGCcjKBVFYXrEtqrhujXk3HDQ6Nn/erc721.html?contract=0xc341cdbF90e13556179271a24e182b831afCF90c&chainId=80001"
         width="100%"
         height="600px"
         frameBorder="0"
         title='NFTDrop'
-      ></iframe>
+      ></iframe> */}
     </div>
   )
 }
